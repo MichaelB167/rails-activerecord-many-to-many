3a98f07983e7285619335bddb5d7b246744ec858
MichaelB167 <michael.m.bates@gmail.com>
2016-12-12 20:44:24 -0500

    Add models for rails many-to-many review

---
+++ app/models/address.rb
@@ -0,0 +1,5 @@
+# documentation
+class Address < ActiveRecord::Base
+  belongs_to :person, inverse_of: :addresses
+  belongs_to :city, inverse_of: :addresses
+end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,7 @@
+# documentation
 class Person < ActiveRecord::Base
+  has_many :cities, through: :addresses
+  has_many :addresses
+  has_many :companies, through: :jobs
+  has_many :jobs
 end
